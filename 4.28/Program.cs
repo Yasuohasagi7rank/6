2b08143d7beb9dc3a0407d13d3d938a85787fff7
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 00:59:47 +0300

    Add files via upload

---
+++ 4.28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = int.Parse(Console.ReadLine());
+            b = a / 100;
+            c = a % 100 / 10;
+            d = a % 10;
+            if (a > 999)
+                Console.WriteLine("Число не трехзначное");
+            else if (b == d)
+                Console.WriteLine("Число является палиндромом");
+            else
+                Console.WriteLine("Число не является палиндромом");
+        }
+    }
+}
